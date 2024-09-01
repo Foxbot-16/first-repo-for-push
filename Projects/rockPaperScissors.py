05fb11e42ed6774c9025104fb73eb7bb60f5e7db
Foxbot-16 <zohiabnaeemrehman@gmail.com>
2024-09-01 17:54:29 +0200

    added play again or quit feature

--- Projects/rockPaperScissors.py
+++ Projects/rockPaperScissors.py
@@ -4,28 +4,39 @@ import random
 # create the variables
 player = None
 options = ["rock", "paper", "scissors"]
+breaker = "p"
 
 # generate the randonm symbol
 computer = random.choice(options)
 
-# get the player input
-player = input("Enter your input: ").lower()
+# print the decorative text
+print("**************************************")
 
-# check if the input is valid
-if player not in options:
-    print("Not a valid input")
+# create the play again
+while breaker == "p":
+    # get the player input
+    player = input("Enter your input: ").lower()
 
-# check the winning condtions
-if player == computer:
-    print("Tie!!")
-elif player == "rock" and computer == "scissors":
-    print("You win")
-elif player == "paper" and computer  == "rock":
-    print("You win")
-elif player == "scissors" and computer == "paper":
-    print("You win")
-else:
-    print("You lose")
+    # check if the input is valid
+    if player not in options:
+        print("Not a valid input")
 
-# print the computer choice
-print(computer)
+    # check the winning condtions
+    if player == computer:
+        print("Tie!!")
+    elif player == "rock" and computer == "scissors":
+        print("You win ✅")
+    elif player == "paper" and computer  == "rock":
+        print("You win ✅")
+    elif player == "scissors" and computer == "paper":
+        print("You win ✅")
+    else:
+        print("You lose ❌")
+
+    # print the computer choice
+    print(f"Computer choice : {computer}")
+
+    # play again option
+    breaker = input("Press P to play again or Q to quit\n").lower()
+
+print("**************************************")
