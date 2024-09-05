c963f26b8a474732d6540f80d730fe4788520880
Foxbot-16 <zohiabnaeemrehman@gmail.com>
2024-09-05 20:47:06 +0200

    modified the files

--- experimental/args_kwargs.py
+++ experimental/args_kwargs.py
@@ -1,3 +1,5 @@
+# working on args
+""" 
 def add(*args):
     total = 0
     for arg in args:
@@ -5,4 +7,12 @@ def add(*args):
     return total
 
 
-print(add(1, 2, 3, 5))
+print(add(1, 2, 3, 5)) 
+"""
+
+#working on kwargs
+def print_address(**kwargs):
+    for key, values in kwargs.items():
+        print(f"the key \"{key}\" has a value \"{values}\"")
+
+print_address(street="Ciccio bello", city="blq", country="USA")
--- experimental/dictionaries.py
+++ experimental/dictionaries.py
@@ -4,10 +4,14 @@ menu = {"pizza": 5.99,
         "lemonade": 4.49,
         "tortilla": 2.99}
 
-
+# print the keys
 for key in menu.keys():
     print(f"this print the key: {key}")
 
-
+# print the values
 for value in menu.values():
     print(f"this print the values: {value}")
+
+# print both
+for key, value in menu.items():
+    print(f"this is the key: {key} with it's value: {value}")
