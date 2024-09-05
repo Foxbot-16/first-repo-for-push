c74d7eef9d820dd374e089ab63dff2cd6d72b511
Foxbot-16 <zohiabnaeemrehman@gmail.com>
2024-09-05 20:28:55 +0200

    adding the experimental forlder

---
+++ experimental/dictionaries.py
@@ -0,0 +1,13 @@
+menu = {"pizza": 5.99,
+        "popcorn": 3.99,
+        "soda": 1.59,
+        "lemonade": 4.49,
+        "tortilla": 2.99}
+
+
+for key in menu.keys():
+    print(f"this print the key: {key}")
+
+
+for value in menu.values():
+    print(f"this print the values: {value}")
